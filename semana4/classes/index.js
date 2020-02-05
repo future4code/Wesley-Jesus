36cf90b9759f34da0ff5cc1d201de960b86a3701
WesleyAraujoBR <wesleyaraujoferminodejesus@gmail.com>
2020-02-05 13:51:21 -0300

    Criação da branch s4-a15, da pasta classes e inclusão dos arquivos index html css e js na referida pasta.

---
+++ semana4/classes/index.js
@@ -0,0 +1,19 @@
+// const filme = {
+//     nome: 'O Ditador',
+//     diretor: 'Larry Charles',
+//     anoLancamento: '2012',
+//     atores: ['Sacha Baron Cohen, Anna Faris, Ben Kingsley'],
+//     filmeAssistido: true,
+
+// }
+
+// const novoFilme = {
+//     ...filme,
+//     nome: 'A Espera de Um Milagre'
+    
+// }
+
+// const filme3 = filme
+// console.log(filme3)
+// console.log(novoFilme)
+
