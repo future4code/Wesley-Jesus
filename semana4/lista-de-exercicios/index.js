98590b95b52690cf76637323a793426157ec2114
WesleyAraujoBR <wesleyaraujoferminodejesus@gmail.com>
2020-02-07 13:44:29 -0300

    Exercício 1 de Interpretação de Códigos concluído.

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,15 @@
+//Resolução dos exercícios de sexta 07 de fevereiro
+
+/*Exercício 1:
+O código deste exercício converte um determinado valor em Dólar para Reais. Há uma função chamada conversorDeMoeda 
+com parâmetro valorEmDolar que faz o seguinte: abre um prompt para o usuário colocar o valor da cotação do dia,
+retornando o valor do produto da cotação pelo valor em dólar. Logo após, há uma constante declarada em que chama a
+função conversorDeMoeda e consta o valor de U$ 100. Será impresso no console o produto de 100 * a cotação digitada
+pelo usuário. 
+
+Por exemplo, a cotação do Dólar no momento está a R$ 4,31. A constante meuDinheiro tem como valor a função
+conversaoDeMoeda e o valor a ser convertido de US$ 100. A função pedirá ao usuário para que ele coloque o 
+valor da cotação, no caso, R$ 4,31. A função realizará o produto dos valores e retornará o resultado. Após
+isso, o console exibirá o valor convertido, que é de R$ 431,00.
+
+Exercício 2:
