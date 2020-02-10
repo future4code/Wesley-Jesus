5f7d1a63ec9929f094d97d69014df08ce2377781
WesleyAraujoBR <wesleyaraujoferminodejesus@gmail.com>
2020-02-10 15:53:59 -0300

    Inserção do endereçamento das imagens.

--- semana5/aula-17/futuretube/src/App.js
+++ semana5/aula-17/futuretube/src/App.js
@@ -23,45 +23,45 @@ function App() {
         <section className="principal" id="section-imgs">
             <figure className="imagens-videos" id="link1">
                 <a href="video1.html" >
-                    <img src={ require("vid1.png") } />
+                    <img src={ require("src/vid1.png") } />
                 </a>
                 <p>Vídeo 1</p>
             </figure>
 
             <figure className="imagens-videos" id="link2">
                 <a href="video2.html" >
-                    <img src={ require("vid2.png") } />
+                    <img src={ require("src/vid2.png") } />
                 </a>
                 <p>Vídeo 2</p>
             </figure>
 
             <figure className="imagens-videos" id="link3">
-                <img src={ require("vid3.png") } />
+                <img src={ require("src/vid3.png") } />
                 <p>Vídeo 3</p>
             </figure>
 
             <figure className="imagens-videos" id="link4">
-                <img src={ require("vid4.png") } />
+                <img src={ require("src/vid4.png") } />
                 <p>Vídeo 4</p>
             </figure>
 
             <figure className="imagens-videos" id="link5">
-                <img src={ require("vid5.png") } />
+                <img src={ require("src/vid5.png") } />
                 <p>Vídeo 5</p>
             </figure>
 
             <figure className="imagens-videos" id="link6">
-                <img src={ require("vid6.png") } />
+                <img src={ require("src/vid6.png") } />
                 <p>Vídeo 6</p>
             </figure>
 
             <figure className="imagens-videos" id="link7">
-                <img src={ require("vid7.png") } />
+                <img src={ require("src/vid7.png") } />
                 <p>Vídeo 7</p>
             </figure>
 
             <figure className="imagens-videos" id="link8">
-                <img src={ require("vid8.png") } />
+                <img src={ require("src/vid8.png") } />
                 <p>Vídeo 8</p>
             </figure>
 
